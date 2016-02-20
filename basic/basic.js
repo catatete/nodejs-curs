35ad9b1607d73bd1be8b69a5277e6ed2864c0809
salboaie <salboaie@gmail.com>
2016-02-20 18:31:25 +0200

    initial examples

---
+++ basic/basic.js
@@ -0,0 +1,12 @@
+var x = 10;
+
+global_variable = 10;
+
+function f(){
+    var x = 11;
+    console.log("X is",x);
+}
+
+console.log("Globa X is",x, f );
+
+//TODO.. apeleaza functia f...
---
+++ syntax/HelloWorld.js
@@ -0,0 +1,5 @@
+//C family
+var x = function(str){
+    console.log();
+}
+x("Hello World");
