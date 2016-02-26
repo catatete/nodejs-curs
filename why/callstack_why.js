54ece41a0b29df75bdf2366225edb973bbcb8c86
salboaie <master1!23>
2016-02-26 19:29:45 +0200

    version increase

--- why/callstack_why.js
+++ why/callstack_why.js
@@ -1,19 +1,23 @@
 var why = require("whys");
 
-function f(){
- throw new Error("Error");
-}
-
-
-function flow(){
-    setTimeout(f.why("Call setTimeout 100"), 100);
-}
-
-
-flow.why("Start the flow ")();
 
 
 process.on('uncaughtException', function (error) {
     console.log(error.stack);
 
 });
+
+function f(){
+    console.error("Throwing...");
+    throw new Error("Error");
+}
+
+
+function flow(){
+    setTimeout.why("Call setTimeout")(f.why("Call f in setTimeout 100"), 100);
+}
+
+
+flow.why("Start the flow ")();
+
+setTimeout(function(){}, 1000);
